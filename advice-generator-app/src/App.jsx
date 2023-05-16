4ccee8f8e9b02e3e005986cb233b19d10a084550
colormethanh <thnguyen1847@gmail.com>
2023-05-16 17:45:13 +0900

    finished project

--- advice-generator-app/src/App.jsx
+++ advice-generator-app/src/App.jsx
@@ -47,7 +47,7 @@ function App() {
 
       <div className="attribution">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-        Coded by <a href="#">Your Name Here</a>.
+        Coded by <a href="#">Thanh Nguyen</a>.
       </div>
     </>
     
