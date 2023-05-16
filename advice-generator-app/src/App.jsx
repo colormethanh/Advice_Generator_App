50e72b79324d1b8f4cf50bba4915dfbf8bd00b20
colormethanh <thnguyen1847@gmail.com>
2023-05-16 18:04:45 +0900

    edited svg and box height

--- advice-generator-app/src/App.jsx
+++ advice-generator-app/src/App.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import './App.css'
 
 function App() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
   const [adviceTitle, setAdviceTitle] = useState("117")
   const [adviceContent, setAdviceContent] = useState("It is easy to sit up and take notice, what's difficult is getting up and taking action.")
 
@@ -14,8 +15,17 @@ function App() {
         setAdviceTitle(resp.data.slip.id)
         setAdviceContent(resp.data.slip.advice)
       })
-
   }
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth)
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", ()=>{
+      handleResize()
+    })
+  })
 
   return (
     <>
@@ -26,12 +36,16 @@ function App() {
           </div>
 
           <div className="advice-content txt-large">
-            {adviceContent}
+            "{adviceContent}"
           </div>
 
           <div className="advice-divider">
-            {/* TODO: Fix SVG */}
-            <svg width="295" height="16" xmlns="http://www.w3.org/2000/svg"><g fill="none" fillRule="evenodd"><path fill="#4F5D74" d="M0 8h122v1H0zM173 8h122v1H173z"/><g transform="translate(138)" fill="#CEE3E9"><rect width="6" height="16" rx="3"/><rect x="14" width="6" height="16" rx="3"/></g></g></svg>
+            {
+              windowWidth > 1200 ? 
+              <svg width="444" height="16" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path fill="#4F5D74" d="M0 8h196v1H0zM248 8h196v1H248z"/><g transform="translate(212)" fill="#CEE3E9"><rect width="6" height="16" rx="3"/><rect x="14" width="6" height="16" rx="3"/></g></g></svg>
+              : 
+              <svg width="295" height="16" xmlns="http://www.w3.org/2000/svg"><g fill="none" fillRule="evenodd"><path fill="#4F5D74" d="M0 8h122v1H0zM173 8h122v1H173z"/><g transform="translate(138)" fill="#CEE3E9"><rect width="6" height="16" rx="3"/><rect x="14" width="6" height="16" rx="3"/></g></g></svg>
+            }
           </div>
           
           <div className="advice-generate">
