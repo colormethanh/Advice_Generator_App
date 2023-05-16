2f705ee278cf423b40a6dfbb18f860a9a2aabd8f
colormethanh <thnguyen1847@gmail.com>
2023-05-16 17:33:00 +0900

    finished coding project

--- advice-generator-app/src/App.jsx
+++ advice-generator-app/src/App.jsx
@@ -3,15 +3,8 @@ import axios from "axios";
 import './App.css'
 
 function App() {
-
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
-  const [adviceTitle, setAdviceTitle] = useState("#117")
+  const [adviceTitle, setAdviceTitle] = useState("117")
   const [adviceContent, setAdviceContent] = useState("It is easy to sit up and take notice, what's difficult is getting up and taking action.")
-
-  const handleWindowResize = () => {
-    console.log("window Resized: " + window.innerWidth)
-    setWindowWidth(window.innerWidth)
-  }
 
   const handleBtnClick = (e) => {
     axios.get("https://api.adviceslip.com/advice")
@@ -23,11 +16,6 @@ function App() {
       })
 
   }
-
-  useEffect(() => {
-    window.addEventListener('resize', handleWindowResize)
-    return () => {window.removeEventListener('resize', handleWindowResize)}
-  })
 
   return (
     <>
@@ -43,18 +31,15 @@ function App() {
 
           <div className="advice-divider">
             {/* TODO: Fix SVG */}
-            {/* <Divider windowWidth={windowWidth}/> */}
             <svg width="295" height="16" xmlns="http://www.w3.org/2000/svg"><g fill="none" fillRule="evenodd"><path fill="#4F5D74" d="M0 8h122v1H0zM173 8h122v1H173z"/><g transform="translate(138)" fill="#CEE3E9"><rect width="6" height="16" rx="3"/><rect x="14" width="6" height="16" rx="3"/></g></g></svg>
-            {/* <svg width="444" height="16" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path fill="#4F5D74" d="M0 8h196v1H0zM248 8h196v1H248z"/><g transform="translate(212)" fill="#CEE3E9"><rect width="6" height="16" rx="3"/><rect x="14" width="6" height="16" rx="3"/></g></g></svg> */}
           </div>
           
           <div className="advice-generate">
             <a href='#' onClick={handleBtnClick}>
-              <div className="advice-btn-container">
+              <div id="dice" className="advice-btn-container">
                 <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z" fill="#202733"/></svg>
               </div>
             </a>
-                     
           </div>
         </div>
 
